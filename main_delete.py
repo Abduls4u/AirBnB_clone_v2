09eb82d8e24a74ee183cc079c70b30f789305451
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-07-29 22:54:36 -0700

    Add: delete() to FileStorage

---
+++ main_delete.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Test delete feature
+"""
+from models.engine.file_storage import FileStorage
+from models.state import State
+from models.city import City
+
+fs = FileStorage()
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+
+# Create a new State
+new_state = State()
+new_state.name = "California"
+fs.new(new_state)
+fs.save()
+print("New State: {}".format(new_state))
+
+# Create a new City
+new_city = City()
+new_city.name = "Ilorin"
+fs.new(new_city)
+fs.save()
+print("New City: {}".format(new_city))
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+
+# Create another State
+another_state = State()
+another_state.name = "Nevada"
+fs.new(another_state)
+fs.save()
+print("Another State: {}".format(another_state))
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])        
+
+# Delete the new State
+fs.delete(new_state)
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+
+
+all_city = fs.all(City)
+print("All Cities: {}".format(len(all_city.keys())))
+for city_key in all_city.keys():
+    print(all_city[city_key])
