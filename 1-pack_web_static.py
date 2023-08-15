30b05254bad14c869eca579cae9bac41c1ff9f44
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-08-15 11:24:54 -0700

    creates a script that generates a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''do_pack module'''
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''generates a .tgz archive from the contents of the web_static'''
+    try:
+        time = datetime.now.strftime('%Y%m%d%H%M%S')
+        archive = 'web_static_' + time + '.tgz'
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(archive))
+        return ('versions/{}'.format(archive))
+    except:
+        return (None)
