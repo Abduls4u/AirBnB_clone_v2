4b3f3a7cfe5d7bd126fb744f197af12543310caf
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-08-15 18:59:31 +0000

    fixes exception error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,10 @@ from datetime import datetime
 def do_pack():
     '''generates a .tgz archive from the contents of the web_static'''
     try:
-        time = datetime.now.strftime('%Y%m%d%H%M%S')
+        time = datetime.now().strftime('%Y%m%d%H%M%S')
         archive = 'web_static_' + time + '.tgz'
         local('mkdir -p versions')
         local('tar -cvzf versions/{} web_static'.format(archive))
         return ('versions/{}'.format(archive))
-    except error:
+    except Exception:
         return (None)
