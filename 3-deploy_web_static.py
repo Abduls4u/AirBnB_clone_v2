4afe2474749403d0182cd1f6f72ad937d812224a
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-08-17 04:26:33 -0700

    fixes import issue

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 '''do_deploy module'''
-from fabric.api import run, put, env
-import os
-from 1-pack_web_static.py import do_pack
-from 2-do_deploy_web_static.py import do_deploy
+from fabric.api import env
+import importlib
+module_name1 = '1-pack_web_static'
+module_name2 = '2-do_deploy_web_static'
+module = importlib.import_module(module_name1)
+module2 = importlib.import_module(module_name2)
+do_pack = module.do_pack
+do_deploy = module2.do_deploy
 env.user = 'ubuntu'
 env.hosts = ['100.26.122.31', '52.3.253.201']
 
 
-def do_deploy():
+def deploy():
     ''' Creates and Distributes an archive to your web servers'''
     try:
         archive_path = do_pack()
