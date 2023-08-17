64c768a3ca6f4ff4123ea929396ab03f8dd308f6
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-08-17 04:07:12 -0700

    Create a script that creates and distributes archives to servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,11 +2,17 @@
 '''do_deploy module'''
 from fabric.api import run, put, env
 import os
-import 1-pack , 2-do_deploy_web_static.py
+from 1-pack_web_static.py import do_pack
+from 2-do_deploy_web_static.py import do_deploy
 env.user = 'ubuntu'
 env.hosts = ['100.26.122.31', '52.3.253.201']
 
 
-def do_deploy(archive_path):
+def do_deploy():
     ''' Creates and Distributes an archive to your web servers'''
-    
+    try:
+        archive_path = do_pack()
+        deploy = do_deploy(archive_path)
+        return (deploy)
+    except Exception:
+        return (False)
