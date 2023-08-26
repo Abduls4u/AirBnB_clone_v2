05a7ae10bf95684852102515e1f87defb85764de
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-08-26 09:34:13 -0700

    starts a Flask web application that displays a text stored in a variable(text)

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application and disolays a text
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''returns Hello'''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''returns HBNB'''
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text_c(text):
+    '''returns C ” followed by the value of the text variable'''
+    return (f"C {text.replace('_', ' ')}")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_text_python(text='is cool'):
+    '''returns Python followed by the value of the text variable'''
+    return (f"Python {text.replace('_', ' ')}")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
